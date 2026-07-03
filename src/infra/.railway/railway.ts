5fbf6fb1873f1ec4ac4780b8bc838195e8851952
Brendan Murty <brendan@bcm.works>
2026-07-03 13:49:05 +1000

    Infra - Update config

--- src/infra/.railway/railway.ts
+++ src/infra/.railway/railway.ts
@@ -38,37 +38,6 @@ export default defineRailway(() => {
       "/commafeed/data": volumeBcmNews,
     },
   });
-  const bcmLinksBrowser = service("bcm-links-browser", {
-    source: image("gcr.io/zenika-hub/alpine-chrome:124"),
-    start: "chromium-browser --headless --no-sandbox --disable-gpu --disable-dev-shm-usage --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --hide-scrollbars",
-    replicas: 1,
-  });
-  const bcmLinksWeb = service("bcm-links-web", {
-    source: image("ghcr.io/karakeep-app/karakeep:release"),
-    replicas: 1,
-    deploy: { limitOverride: { containers: { cpu: 8, memoryBytes: 8000000000 } } },
-    env: {
-      BROWSER_WEB_URL: preserve(),
-      DISABLE_SIGNUPS: preserve(),
-      INFERENCE_IMAGE_MODEL: preserve(),
-      INFERENCE_TEXT_MODEL: preserve(),
-      MEILI_ADDR: preserve(),
-      MEILI_MASTER_KEY: preserve(),
-      NEXTAUTH_SECRET: preserve(),
-      NEXTAUTH_URL: preserve(),
-      OPENAI_API_KEY: preserve(),
-      PORT: preserve(),
-      URL: preserve(),
-    },
-  });
-  const bcmLinksSearch = service("bcm-links-search", {
-    source: image("getmeili/meilisearch:v1.41.0"),
-    replicas: 1,
-    env: {
-      MEILI_MASTER_KEY: preserve(),
-      MEILI_NO_ANALYTICS: preserve(),
-    },
-  });
   const bcmSite = service("bcm-site", {
     source: image("brendanmurty/bcm-site:latest"),
     healthcheck: "/health",
@@ -96,6 +65,6 @@ export default defineRailway(() => {
   });
 
   return project("bcm", {
-    resources: [bcmGit, bcmNews, bcmLinksBrowser, bcmLinksWeb, bcmLinksSearch, bcmSite, volumeBcmNews, volumeBcmGit],
+    resources: [bcmGit, bcmNews, bcmSite, volumeBcmNews, volumeBcmGit],
   });
 });
