dcae372b70edecdd204c58f360839cffbce6a586
Brendan Murty <brendan@bcm.works>
2026-07-22 15:13:04 +1000

    Cleanup prototypes

--- prototypes/deno-fresh/utils/state.ts
+++
@@ -1,6 +0,0 @@
-import { createDefine } from "fresh";
-import { AppState } from "@/types/state.type.ts";
-
-export type State = AppState;
-
-export const define = createDefine<State>();
--- prototypes/deno-fresh/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import { fresh } from "@fresh/plugin-vite";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  // @ts-ignore This is the valid default setup for Fresh.
-  plugins: [fresh(), tailwindcss()],
-});
