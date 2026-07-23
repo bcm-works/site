7e34ed8d8ef03adefecd5a2580a101f7702aa14d
Brendan Murty <brendan@bcm.works>
2026-07-23 16:47:04 +1000

    Progress on Eleventy prototype

--- prototypes/eleventy/eleventy.config.js
+++ prototypes/eleventy/eleventy.config.js
@@ -3,14 +3,23 @@ import toml from "@iarna/toml";
 // Configure Eleventy
 export default function(eleventyConfig) {
 	eleventyConfig.setInputDirectory("content");
-	eleventyConfig.setOutputDirectory("public");
 
+	// These paths must be relative to the Input Directory
 	eleventyConfig.setIncludesDirectory("../src/includes");
 	eleventyConfig.setLayoutsDirectory("../src/templates");
 
-	eleventyConfig.addPassthroughCopy();
+	eleventyConfig.setOutputDirectory("public");
 
-	// eleventyConfig.setQuietMode(true);
+	// Copy "src/styles" to the Output Directory as a sub directory named "css"
+	eleventyConfig.addPassthroughCopy({ "src/styles": "css" });
+
+	eleventyConfig.addPassthroughCopy({ "content/images": "images" });
+	eleventyConfig.addPassthroughCopy({ "content/favicon.ico": "favicon.ico" });
+	eleventyConfig.addPassthroughCopy({ "content/resume.pdf": "resume.pdf" });
+	eleventyConfig.addPassthroughCopy({ "src/pwa-manifest.json": "manifest.json" });
+	// TODO: post json file goes here too - "posts.json": "posts.json"
+
+	eleventyConfig.setQuietMode(true);
 
 	eleventyConfig.setFrontMatterParsingOptions({
 		engines: {
