79c5a36f43187e75a28b6ab33f80a272a90434d3
Brendan Murty <brendan@bcm.works>
2026-07-29 10:44:04 +1000

    Remove Eleventy prototype

--- prototypes/eleventy/tools/docker/docker-stop.ts
+++
@@ -1,6 +0,0 @@
-import { execSync as run } from 'node:child_process';
-import { warn } from '#tool/log';
-
-warn("Stopping and removing current 'bcm-site' container");
-
-run(`docker stop bcm-site || true && docker rm bcm-site || true`);
