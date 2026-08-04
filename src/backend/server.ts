668835b3651a85a72905d914206dcb01cd75af40
Brendan Murty <brendan@bcm.works>
2026-08-04 12:24:55 +1000

    Remove problematic request domain check in server

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -30,12 +30,6 @@ Deno.serve(
     let path: string = url.pathname === "/" ? "/index.html" : url.pathname;
     path = path.endsWith("/") ? path.slice(0, -1) : path;
 
-    // If the request came from another source, redirect to the correct URL
-    if (path != "/api/health" && url.origin != siteUrl) {
-      bcm.postHogAnonBackendEvent(301, request);
-      return Response.redirect(new URL(siteUrl), 301);
-    }
-
     // Handle static file requests
     const fileStatic: string = `./${publicDir}${path}`;
     if (fileExists(fileStatic)) {
