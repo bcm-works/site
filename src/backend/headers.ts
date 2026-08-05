ee7bbbfd0f27f1441e667040a569dcd6f5f5a8e4
Brendan Murty <brendan@bcm.works>
2026-08-05 14:59:37 +1000

    Update Server CORS header setup

--- src/backend/headers.ts
+++ src/backend/headers.ts
@@ -3,12 +3,19 @@ import { Env } from "@/common/env.ts";
 const env = new Env();
 const siteUrl: string = env.getUrl();
 
+// Define the request origin domains that will be allowed by CORS
+const allowedOrigins = new Set([
+  siteUrl,
+  "https://murty.au",
+  "https://bcm.id.au",
+]);
+
 // Setup headers for CORS (Cross-Origin Resource Sharing)
 export function cors(req: Request): Headers {
   const headers = new Headers();
   const origin = req.headers.get("origin");
 
-  if (origin && origin == siteUrl) {
+  if (origin && allowedOrigins.has(origin)) {
     headers.set("access-control-allow-origin", origin);
     headers.set("vary", "origin");
   }
