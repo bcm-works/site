df415a47fd81af5358cb123cfa7eb5260f001d15
Brendan Murty <brendan@bcm.works>
2026-08-05 13:22:58 +1000

    Simplify server CORS header logic

--- src/backend/headers.ts
+++ src/backend/headers.ts
@@ -3,19 +3,12 @@ import { Env } from "@/common/env.ts";
 const env = new Env();
 const siteUrl: string = env.getUrl();
 
-// Define the request origin domains that will be allowed by CORS
-const allowedOrigins = new Set([
-  siteUrl,
-  "https://bcm-site.murty.deno.net/",
-  "http://localhost:8000/"
-]);
-
 // Setup headers for CORS (Cross-Origin Resource Sharing)
 export function cors(req: Request): Headers {
   const headers = new Headers();
   const origin = req.headers.get("origin");
 
-  if (origin && allowedOrigins.has(origin)) {
+  if (origin && origin == siteUrl) {
     headers.set("access-control-allow-origin", origin);
     headers.set("vary", "origin");
   }
