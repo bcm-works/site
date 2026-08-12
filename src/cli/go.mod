d734f5508d6482f2d7003f6fc3ae7de3ea46763f
Brendan Murty <brendan@bcm.works>
2026-08-12 21:48:32 +1000

    Update dependencies, and improve tooling in this area

--- src/cli/go.mod
+++ src/cli/go.mod
@@ -7,7 +7,7 @@ require github.com/fatih/color v1.19.0 // direct
 require github.com/joho/godotenv v1.5.1 // direct
 
 require (
-	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.44.0 // indirect
+	github.com/mattn/go-colorable v0.1.15 // indirect
+	github.com/mattn/go-isatty v0.0.24 // indirect
+	golang.org/x/sys v0.47.0 // indirect
 )
--- src/cli/task_deps.go
+++ src/cli/task_deps.go
@@ -4,6 +4,8 @@ func TaskDeps() {
 	LogInfo("Updating Deno dependencies")
 	Cmd("deno task update")
 
-	LogInfo("Committing the results")
-	Cmd("git add deno.json deno.lock && git commit -m \"Update third-party dependencies\" || true")
+	LogInfo("Updating Go dependencies")
+	Cmd("cd src/cli && go get -u -t ./... && go mod tidy")
+
+	LogWarn("If there are Git diffs now, a CLI build then a test run must be run locally before committing and pushing changes")
 }
