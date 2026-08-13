22cdee0f3d7bd51d8a355d77c252c408124d968a
Brendan Murty <brendan@bcm.works>
2026-08-13 16:45:45 +1000

    Start on Go backend prototype

---
+++ prototypes/go-backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/bcm-works/site/backend/v2
+
+go 1.26.5
+
+require github.com/go-chi/chi/v5 v5.3.1 // indirect
---
+++ prototypes/go-backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	fmt.Println("Server starting at http://localhost:3000")
+	http.ListenAndServe(":3000", r)
+}
