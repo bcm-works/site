6ca9f6548391b097f4556e7fe90bba6bcf983ded
Brendan Murty <brendan@bcm.works>
2026-08-27 10:38:29 +1000

    Copy over previous Links prototype from PR#14

---
+++ prototypes/links-karakeep/previous/bin/stop-links.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+#
+# Stop the local version of Links
+#  - Run via: just links-stop
+#
+#
+
+LINKS_DIR="$(cd "$(dirname "$0")" && cd .. && pwd)"
+
+cd "$LINKS_DIR"
+
+docker compose down
